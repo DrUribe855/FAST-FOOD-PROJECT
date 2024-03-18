32fbfa1a77da55138fa81d4451f67808cb9c909b
Mauricio <mauricioaguirre2490@gmail.com>
2024-03-18 18:30:55 -0500

    Add vuetify

--- fastFood/resources/js/app.js
+++ fastFood/resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import Vuetify from "vuetify";
+
+Vue.use(Vuetify);
+ 
+window.Vue = require('vue');
+
+Vue.component('login', require('./components/Login.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+});
---
+++ fastFood/resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <title>Login</title>
+</head>
+<body>
+    <div id="app">
+        <login></login>
+    </div>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}"> </script>
+</body>
+</html>
