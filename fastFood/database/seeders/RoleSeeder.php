28850b724204878fcd983c255cc628c197e7c72b
Mauricio <mauricioaguirre2490@gmail.com>
2024-03-19 15:19:29 -0500

    Add RoleSeeder

--- fastFood/database/seeders/RoleSeeder.php
+++ fastFood/database/seeders/RoleSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder
 {
@@ -14,6 +16,9 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $roleAdmin = Role::create(['name' => 'Admin']);
+        $roleUser = Role::create(['name' => 'User']);
+
+        // Permission::create(['name' => 'nombre del permiso']);
     }
 }
