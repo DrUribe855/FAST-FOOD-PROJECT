bfa1a2013ffb95a5e052d77efbc1b9a4509957cb
xmonsox <tareas694@gmail.com>
2024-03-19 10:53:47 -0500

    Register medium

--- fastFood/app/Http/Controllers/UserController.php
+++ fastFood/app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,4 +9,22 @@ class UserController extends Controller
     public function index(){
         return view('login');
     }
+    public function RegisterUser(Request $request)
+    {
+        $users = new User();
+        $users->document = $request->input('document');
+        $users->name = $request->input('name');
+        $users->phone_number = $request->input('phone_number');
+        $users->email = $request->input('email');
+        $users->status = $request->input('status');
+        $users->password = $request->input('password');
+        $users->save();
+
+        $data = [
+            'status' => true,
+            'users' => $users
+        ];
+        return response()->json($data);
+    }
+    
 }
--- fastFood/routes/web.php
+++ fastFood/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index']);
+Route::post("/RegisterUser", [UserController::class, "RegisterUser"])->name("user.register");
