5f7d436bd99af38b04c3777e21d0f1789e16a1e0
Dr.Uribe <yeisonl2036@gmail.com>
2024-04-02 18:53:41 -0500

    Middleware

--- fastFood/routes/web.php
+++ fastFood/routes/web.php
@@ -14,11 +14,11 @@ Route::post('/login', [LoginController::class, 'verifyUserData'])->name('login')
 Route::get('/logout', [LoginController::class, 'logout']);
 
 // Categories
-Route::get('/categorie', [CategoryController::class, 'index'])->middleware('can: admin.generalShow');
-Route::get('/getCategorie', [CategoryController::class, 'getCategorie'])->middleware('can: admin.generalShow');
-Route::post('/newCategorie', [CategoryController::class, 'insertCategorie'])->middleware('can: admin.insert');
-Route::post('/editCategorie', [CategoryController::class, 'editCategorie'])->middleware('can: admin.update');
-Route::post('/searchCategorie', [CategoryController::class, 'searchCategorie'])->middleware('can: admin.generalShow');
+Route::get('/categorie', [CategoryController::class, 'index']); //->middleware('can: admin.generalShow');
+Route::get('/getCategorie', [CategoryController::class, 'getCategorie']); //->middleware('can: admin.generalShow');
+Route::post('/newCategorie', [CategoryController::class, 'insertCategorie']); //->middleware('can: admin.insert');
+Route::post('/editCategorie', [CategoryController::class, 'editCategorie']); //->middleware('can: admin.update');
+Route::post('/searchCategorie', [CategoryController::class, 'searchCategorie']); //->middleware('can: admin.generalShow');
 
 
 //Ver Formulario de Registro de Usuario
@@ -28,17 +28,17 @@ Route::get('/userRegistration', [UserController::class, 'showRegister'])->name('
 Route::post("/RegisterUser", [UserController::class, "registerUser"])->name("user.register");
 
 //productos
-Route::get('/consultProduct/{id}', [ProductController::class, 'ShowProductsIndivual'])->name('consultProduct')->middleware('can: admin.generalShow');
-Route::get('/consult', [ProductController::class, 'viewConsult'])->name('view')->middleware('can: admin.generalShow');
-Route::get('/viewProducts', [ProductController::class, 'viewProducts'])->name('viewP')->middleware('can: admin.generalShow');
-Route::get('/showProducts', [ProductController::class, 'ShowProducts'])->name('showproducts')->middleware('can: admin.generalShow');
-Route::post('/registerProduct', [ProductController::class, 'registerProduct'])->name('registerProduct')->middleware('can: admin.insert');
-Route::put("/UpdateProduct/{id}", [ProductController::class, 'updateProducto'])->name('updateProd')->middleware('can: admin.update');
-Route::get('/ExtractCategories', [ProductController::class, 'getCategorys'])->middleware('can: admin.generalShow    ');
+Route::get('/consultProduct/{id}', [ProductController::class, 'ShowProductsIndivual'])->name('consultProduct'); //->middleware('can: admin.generalShow');
+Route::get('/consult', [ProductController::class, 'viewConsult'])->name('view'); //->middleware('can: admin.generalShow');
+Route::get('/viewProducts', [ProductController::class, 'viewProducts'])->name('viewP'); // ->middleware('can: admin.generalShow');
+Route::get('/showProducts', [ProductController::class, 'ShowProducts'])->name('showproducts'); //->middleware('can: admin.generalShow');
+Route::post('/registerProduct', [ProductController::class, 'registerProduct'])->name('registerProduct'); //->middleware('can: admin.insert');
+Route::put("/UpdateProduct/{id}", [ProductController::class, 'updateProducto'])->name('updateProd'); //->middleware('can: admin.update');
+Route::get('/ExtractCategories', [ProductController::class, 'getCategorys']);// ->middleware('can: admin.generalShow    ');
 
 // Registro de ventas
-Route::get('/sellsView', [BillController::class, 'billsView'])->middleware('can: admin.generalShow');
-Route::get('/getBills', [BillController::class, 'getBills'])->middleware('can: admin.generalShow');
+Route::get('/sellsView', [BillController::class, 'billsView']);// ->middleware('can: admin.generalShow');
+Route::get('/getBills', [BillController::class, 'getBills']);// ->middleware('can: admin.generalShow');
 
 
 
