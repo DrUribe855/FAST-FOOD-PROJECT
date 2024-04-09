4771b9e873f12440adccd4425be41e8da52b27d2
xFranco10 <andreyfranco109@gmail.com>
2024-04-09 09:49:53 -0500

    Adding Images and Adding CartShop

--- AppFastFood/app/src/main/java/com/example/appfastfood/LoginActivity.java
+++ AppFastFood/app/src/main/java/com/example/appfastfood/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.appfastfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,7 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         btnIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validarIngreso();
+                Intent intencion = new Intent(LoginActivity.this, CartListActivity.class);
+                startActivity(intencion);
             }
         });
     }
